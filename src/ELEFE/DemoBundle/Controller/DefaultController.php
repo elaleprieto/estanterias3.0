776c8c28a53defd78ee976d79a0b8d224a586203
Ale Prieto <aleprieto@gmail.com>
2012-07-03 13:29:41 -0300

    Pruebas de Layout

--- src/ELEFE/DemoBundle/Controller/DefaultController.php
+++ src/ELEFE/DemoBundle/Controller/DefaultController.php
@@ -4,12 +4,65 @@ namespace ELEFE\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+class DefaultController extends Controller {
 
-class DefaultController extends Controller
-{
-    
-    public function indexAction($name)
-    {
-        return $this->render('ELEFEDemoBundle:Default:index.html.twig', array('name' => $name));
-    }
+	public function indexAction($name) {
+		return $this -> render('ELEFEDemoBundle:Default:index.html.twig', array('name' => $name));
+	}
+
+	public function articulosAction() {
+		//-- Simulamos obtener los datos de la base de datos cargando los artículos a un array
+		$articulos = array(
+			array(
+				'id' => 1,
+				'title' => 'Articulo numero 1',
+				'created' => '2011-01-01'
+			),
+			array(
+				'id' => 2,
+				'title' => 'Articulo numero 2',
+				'created' => '2011-01-01'
+			),
+			array(
+				'id' => 3,
+				'title' => 'Articulo numero 3',
+				'created' => '2011-01-01'
+			),
+		);
+		return $this -> render('ELEFEDemoBundle:Default:articulos.html.twig', array('articulos' => $articulos));
+	}
+
+	public function articuloAction($id) {
+		//-- Simulamos obtener los datos de la base de datos cargando los artículos a un array
+		$articulos = array(
+			array(
+				'id' => 1,
+				'title' => 'Articulo numero 1',
+				'created' => '2011-01-01'
+			),
+			array(
+				'id' => 2,
+				'title' => 'Articulo numero 2',
+				'created' => '2011-01-01'
+			),
+			array(
+				'id' => 3,
+				'title' => 'Articulo numero 3',
+				'created' => '2011-01-01'
+			),
+		);
+
+		//-- Buscamos dentro del array el ID solicitado
+		$articuloSeleccionado = null;
+		foreach ($articulos as $articulo) {
+			if ($articulo['id'] == $id) {
+				$articuloSeleccionado = $articulo;
+				break;
+			}
+		}
+
+		//-- Invocamos a nuestra nueva plantilla, pasando los datos
+		return $this -> render('ELEFEDemoBundle:Default:articulo.html.twig', array('articulo' => $articuloSeleccionado));
+	}
+
 }
